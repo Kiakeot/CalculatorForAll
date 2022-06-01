e39bd38e74de9a8b009efd5695cc26a526cfa5cc
BogdanStarunskiy <bogdan.starunskiy@gmail.com>
2022-06-01 17:05:50 +0300

    ui theme fix

--- app/src/main/java/com/example/calculatorforall/MainActivity.java
+++ app/src/main/java/com/example/calculatorforall/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Switch;
